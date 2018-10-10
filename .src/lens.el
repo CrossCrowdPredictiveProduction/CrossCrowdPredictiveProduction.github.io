8f92c2a0dcd8ef47a716bda7e0a2753c2686441f
Patrick Anderson <Patrick.Anderson@EchoStar.com>
2018-10-09 18:32:47 -0700

    CityCoin.  lens.el: do not link to self

--- .src/lens.el
+++ .src/lens.el
@@ -22,23 +22,21 @@
 
 
 ;;; Features
-;; Link plain-text documents without markup;
-;; preserve whitespace; guess formatting
+;; Link plain-text documents without markup.
+;; Preserve whitespace and guess formatting.
 
 ;;  Header: =, ==, ===, ====
 ;;  Shell: $, #
 ;;  Bullet: :, *, .
 ;;  Quote: >, >>, "''"
-;;  Comment: //, #, ;;, /**/
 ;;  Paren: (), {}, []
-;;  Path: ~/, ./LocalFile, ../LocalFile, GNU.org
+;;  Implicit: GNU.org
+;;  Explicit: gopher://GNU.org
 
 ;;; Installation
-
-;; Copy this file to directory like ~/doc/.src/lens.el
+;; Copy this file somewhere like ~/doc/.src/lens.el
 
 ;; Put this in your .emacs:
-
 ;; (autoload 'lens-mode "~/doc/.src/lens.el" "lens" t)
 ;; (autoload 'lens-mode "lens" "" t)
 
@@ -48,12 +46,18 @@
 ;;        auto-mode-alist))
 
 ;;; Example scenario
-;; Open ~/doc/.txt/profit to define that term.
-;; While editing that file in Emacs:  M-x lens
-;; The output file will be ~/doc/profit.htm
-;; All lowercase in filename to always match.
+;; Write ~/doc/.txt/profit to define that term.
+;; While editing that file in Emacs, run  M-x lens
+;; Output is written to  ~/doc/profit.htm
 
-;;  Main entry points are `lens', `lens-mode', `lens-make-page'
+
+;;; Limitations
+;; Filenames must be all lowercase and
+;; will match case variations in text.
+
+
+;;;  Main entry points
+;; `lens', `lens-mode', `lens-make-page'
 ;;  `lens-dired-make-marked-pages' and `lens-clean'
 
 ;;  In `lens-mode': RET or `mouse-2' at beginning of term visits that file.
@@ -61,48 +65,15 @@
 ;; `lens-mode' keys:
 ;; f5       `lens-mode-rebuild-mode'
 ;; C-cC-c   `lens-make-page'
-;; C-cC-p   preview generated HTML of this source file
+;; C-cC-p   Preview generated HTML of this source file
 ;; RET      Follow link
 ;; C-m      Insert line-feed
 
-;;;; Features
-
-;;; Formatting
-;; All whitespace preserved
-
-;;; At beginning of line (BOL)
-;;  = Header1
-;;  == Header2
-;;  === Header3
-;;  ==== Header4
-;;  $ shell command
-;;  : bullet
-;;  * bullet
-;;  + bullet
-;;  . bullet
-;;  > quote
-
-;;; Anywhere
-;;  >> quote2
-;;  >>> quote3
-;;  "'quote'"
-;;  /* comment */
-;;  ;; comment
-;;  (parenthetic)
-;;  {parenthetic}
-;;  [parenthetic]
-;;  \\UNC-Server\Shared_Folder
-;;  "\\UNC-Server\Shared Folder"
-;;  ~/LocalFile
-;;  ./LocalFile
-;;  ../LocalFile
-
-;; Implicit URLs: `lens-implicit-HTTP'
-;; Explicit URLs: [file|https?|ftp]://
 
 ;;; Version:
 ;; .01 new
 ;; .02 cleanup
+;; .03 do not link to self
 
 ;;; Customizations:
 (defcustom lens-global-page-title nil "Title of _all_ generated pages.  If set to `nil' the title is the name of the input file.")
@@ -124,44 +95,34 @@ All terms less than this match only at the beginning of words (using `\\b')")
 (require 'time-date)
 (require 'interpreter-minor)
 
-;; The following effect only `font-lock'
-;; KLUDGE: see `lens-build-mapping' for HTML regexps
-(defconst lens-quoted-local-M$-path
-  "\"\\([a-zA-Z]:\\\\.+?\\)\""
-  "Quoted path on local hard drive")
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-(defconst lens-local-M$-path
-  "[a-zA-Z]:\\\\[^]):,;? \t\n]+"
-  "Path on local hard drive")
+;; ;; The following effect only `font-lock'
+;; ;; see `lens-build-mapping' for HTML regexps
+;; (defconst lens-explicit-URL
+;;   "\\([a-zA-Z0-9]\\)+://[^ \t\n]*"
+;;   "This allows illegal domain names and
+;; includes chars 'banned' from implicit-HTTP.")
 
-(defconst lens-quoted-UNC-path
-  "\"\\(\\\\\\\\[^]:?]+?\\)\""
-  "Quoted UNC")
-
-(defconst lens-UNC-path
-  "\\\\\\\\[^]):,;? \t\n]+"
-  "UNC path on MS network")
-
-(defconst lens-*nix-path
-  "\\(\\.\\./\\|\\./\\)[^|> \t\n\r]*"
-  "../ or ./ followed by anything but |, >, or whitespace")
-
-(defconst lens-*nix-path-full
-  (concat "<a class=\"ext\" href=\"file:///" default-directory "\\&\">\\&</a>"))
-
-(defconst lens-explicit-URL
-  "\\([a-zA-Z0-9]\\)+://[^ \t\n]*"
-  "This is to include chars banned from implicit-HTTP.")
-
-(defconst lens-email-link
-  "\\([a-zA-Z0-9\.]\\)+@[^])}>:,; \t\n]*"
-  "email addrs")
+;; (defconst lens-implicit-HTTP
+;;   ;;"\\(\\([a-zA-Z0-9-]\\)+\\.\\)+\\([a-zA-Z][a-zA-Z]\\)[^])}>:,; \t\n]*"
+;;   "\\(\\([a-zA-Z0-9-]\\)+\\.\\)+[^])}>:,; \t\n]*"
+;;   ;;maybe use `regexp-opt' here?
+;;   "This only allows legal domain names.
+;; I don't know what the  [a-zA-Z][a-zA-Z]  part does.
+;; Characters  ])}>:,; \\t\\\n  end implicit URL.
+;; ")
 
 (defconst lens-implicit-HTTP
   "\\(\\([a-zA-Z0-9_-]\\)+\\.\\)+\\([a-zA-Z][a-zA-Z]\\)[^])}>:,; \t\n]*"
   ;;maybe use `regexp-opt' here?
   "Characters ])}>:,; \\t\\\n end the implicit HTTP URL.")
 
+(defconst lens-explicit-URL
+  "\\([a-zA-Z0-9]\\)+://[^ \t\n]*"
+  "This is to include chars banned from implicit-HTTP.")
 
 (defvar lens-mapping nil)
 (defvar lens-input-dirs '("."))
@@ -210,35 +171,31 @@ All terms less than this match only at the beginning of words (using `\\b')")
 			lens-mapping))))
 
   (dolist
-	  (term
+	  (term ;; regexp followed by HTML markup and optional HTML closing markup
 	   '(
-		 ;;The first entry here has LOWEST priority.
+         ;;;; NOTE: Entries at the BOTTOM of this list have HIGHEST priority
 
 		 ;;must appear at BOL
 		 ;;   		 ("^.[^ \n\t]+?:" "<span class=\"type\">\\&" "</span>")
 
 		 ;;  		 ("^.*?:" "<span class=\"h4\">\\&" "</span>");anything followed by a : is a title?
 
-         ("^Related:" "<span class=\"rel\">\\&</span>")
- 		 ;;("\\(^Related:\\)\\(.*\\)" "<span class=\"rel tiny\">\\1</span><span class=\"tiny\">\\2</span>")
-		 ("^[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}:" "<hr/><span class=\"date\">\\&</span>")
- 		 ("^>" "<span class=\"quot\">\\&" "</span>")
+         ("^Related:"  "<span class=\"rel\">\\&</span>")
+         ;; This is for the diary date markup
+		 ("^[0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}:"  "<hr/><span class=\"date\">\\&</span>")
+ 		 ("^>"  "<span class=\"quot\">\\&"  "</span>")
 
- 		 ("^=" "<span class=\"h1\">\\&" "</span>")
- 		 ("^==" "<span class=\"h2\">\\&" "</span>")
- 		 ("^===" "<span class=\"h3\">\\&" "</span>")
- 		 ("^====" "<span class=\"h4\">\\&" "</span>")
- 		 ;; ("^----" "<hr/>\\&")
+ 		 ("^="  "<span class=\"h1\">\\&"  "</span>")
+ 		 ("^=="  "<span class=\"h2\">\\&"  "</span>")
+ 		 ("^==="  "<span class=\"h3\">\\&"  "</span>")
+ 		 ("^===="  "<span class=\"h4\">\\&"  "</span>")
+ 		 ;; ("^----"  "<hr/>\\&")
 
- 		 ("^\\$ " "<span class=\"shell\">\\&" "</span>")
+ 		 ("^\\$ "  "<span class=\"shell\">\\&"  "</span>")
 
-		 ;;("^[ \t]*\\([0-9]\\|[A-Z]\\|[a-z]\\)+\\(\\.\\|:\\)$" "<span class=\"bullet\">\\&</span>")
-
- 		 ("^[ \t]*\\(:\\|\\*\\|\\.\\)" "<span class=\"bullet\">\\&</span>")
-		 ;;("^[ \t]*:" "<span class=\"indent\">\\&</span>")
-
- 		 ("^::" "<span class=\"cmnt\">\\&" "</span>") ;;Batch-file comments
-		 ;;  		 ("^REM" "<span class=\"cmnt\">\\&" "</span>") ;;Batch-file comments
+         ;; If the first char is a number or any of  :*.o
+ 		 ;; ("^[ \t]*\\([0-9]\\|:\\|\\*\\|\\.\\|o\\)" "<span class=\"bullet\">\\&</span>")
+         ("^[ \t]*\\(:\\|\\*\\|\\.\\)" "<span class=\"bullet\">\\&</span>")
 
  		 ("(\\|{\\|\\[" "<small>\\&")
  		 (")\\|\\}\\|]" "\\&</small>")
@@ -256,48 +213,17 @@ All terms less than this match only at the beginning of words (using `\\b')")
 		 ("  " "&nbsp;&nbsp;")
 		 ("^ " "&nbsp;")
 
-		 ;;remote images
-  		 (";;" "<span class=\"cmnt\">\\&" "</span>")
  		 (">>" "<span class=\"quot2\">\\&" "</span>")
  		 (">>>" "<span class=\"quot3\">\\&" "</span>")
-
-		 ;;KLUDGE: how do I expand these vars here?
-		 ;;see `lens-quoted-local-M$-path'
-		 ("\"\\([a-zA-Z]:\\\\.+?\\)\""
-		  "<a class=\"ext\" href=\"file:///\\1\">\\&</a>")
-
-		 ;;see `lens-local-M$-path'
-		 ("[a-zA-Z]:\\\\[^]):,;? \t\n]+"
-		  "<a class=\"ext\" href=\"file:///\\&\">\\&</a>")
-
-		 ;;see `lens-quoted-UNC-path'
-		 ("\"\\(\\\\\\\\[^]:?]+?\\)\""
-		  "<a class=\"ext\" href=\"file:///\\1\">\\&</a>")
-
-		 ;;see `lens-UNC-path'
-		 ("\\\\\\\\[^]):,;? \t\n]+"
-		  "<a class=\"ext\" href=\"file:///\\&\">\\&</a>")
-
-		 ;;see 'lens-*nix-path'
-		 ("\\(\\.\\./\\|\\./\\)[^|> \t\n\r]*"
-		  "<a class=\"ext\" href=\"\\&\">\\&</a>")
-		  ;'lens-*nix-path-full)
-		  ;(concat "<a class=\"ext\" href=\"file:///" default-directory "\\&\">\\&</a>"))
-		  ;"<a class=\"ext\" href=\"file:///root/work/doc/.src/\\&\">\\&</a>")
 
 		 ;;TODO: encode & to &amp;
 		 ;;see `lens-explicit-URL'
 		 ("\\([a-zA-Z0-9]\\)+://[^ \t\n]*"
 		  "<a class=\"ext\" href=\"\\&\">\\&</a>")
 
-		 ;;TODO: encode & to &amp;
-		 ;;see `lens-email-link'
-		 ("\\([a-zA-Z0-9\.]\\)+@[^])}>:,; \t\n]*"
-		  "<a class=\"eml\" href=\"mailto:\\&\">\\&</a>")
-
 		 ;;see `lens-implicit-HTTP'
-		 ;("\\(\\([a-zA-Z0-9_-]\\)+\\.\\)+\\(aero\\|at\\|au\\|be\\|biz\\|ca\\|cat\\|cc\\|ch\\|com\\|coop\\|cx\\|cz\\|da\\|de\\|dk\\|edu\\|es\\|eu\\|fi\\|fr\\|gov\\|hu\\|ie\\|il\\|im\\|in\\|it\\|info\\|int\\|io\\|jp\\|jobs\\|lt\\|me\\|mil\\|mobi\\|museum\\|name\\|net\\|nl\\|nu\\|nz\\|org\\|pl\\|pro\\|pt\\|ro\\|ru\\|se\\|si\\|sk\\|tel\\|to\\|travel\\|tv\\|uk\\|us\\|ws\\|za\\)[^])}>:,; \t\n]*"
-          ("\\(\\([a-zA-Z0-9_-]\\)+\\.\\)+\\([a-zA-Z][a-zA-Z]\\)[^])}>:,; \t\n]*"
+         ("\\(\\([a-zA-Z0-9_-]\\)+\\.\\)+\\([a-zA-Z][a-zA-Z]\\)[^])}>:,; \t\n]*"
+          ;("\\(\\([a-zA-Z0-9_-]\\)+\\.\\)+[^])}>:,; \t\n\j]*"
 		  "<a class=\"ext\" href=\"http://\\&\">\\&</a>")
 
 		 ;;XML entities
@@ -314,7 +240,7 @@ All terms less than this match only at the beginning of words (using `\\b')")
 		 ("\n" "<br/>\n")
 
 ;;;;;;;; ---- ADD TERMS ABOVE THIS LINE ---- ;;;;;;;;
-;;;;;;;; Note: Entries at the BOTTOM of this list have HIGHEST priority
+         ;;;; NOTE: Entries at the BOTTOM of this list have HIGHEST priority
 		 ))
     (setq lens-mapping (cons term lens-mapping))))
 
@@ -384,6 +310,7 @@ All terms less than this match only at the beginning of words (using `\\b')")
             (setq case-fold-search t)
             (goto-char (point-min))
             (let ((term-count (length lens-mapping)))
+
               (while (< (point) (point-max)) ;;while not EOF
                 (let ((cur-point (point))
                       (max-point (point-max))
@@ -394,7 +321,8 @@ All terms less than this match only at the beginning of words (using `\\b')")
                     (let ((regexp (car (nth cur-term lens-mapping)))
 						  (replacement (car (cdr (nth cur-term lens-mapping))))
 						  (closing (cdr (cdr (nth cur-term lens-mapping)))))
-                      (if (looking-at regexp)
+                      (if (and (looking-at regexp)
+                               (not (string-equal file regexp))) ;; do not link to self
                           (progn ;;found the term
 
 							;;if `cur-term' represents a file (not just a regexp)
@@ -525,13 +453,7 @@ All terms less than this match only at the beginning of words (using `\\b')")
   ;;encase and terminate
   (setq lens-font-lock
         (concat "\\(\\)\\("
-				lens-quoted-local-M$-path "\\|"
-				lens-local-M$-path "\\|"
-				lens-quoted-UNC-path "\\|"
-				lens-UNC-path "\\|"
-				lens-*nix-path "\\|"
 				lens-explicit-URL "\\|"
-				lens-email-link "\\|"
 				lens-implicit-HTTP "\\|"
 				lens-font-lock "zzzzzzzz\\)")))
 ;; what a mess.
@@ -558,11 +480,13 @@ All terms less than this match only at the beginning of words (using `\\b')")
 (defun lens-mode-rebuild-mode ()
   (interactive)
   (lens-build-font-lock)
-  (im-make-mode lens "lens --- focus on text." lens-font-lock lens-follow)
+  ;; something wrong with lens-font-lock here,
+  ;; so sending  nil  instead
+  (im-make-mode lens "lens --- focus on text." nil lens-follow)
   (lens-mode))
 
-;; KLUDGE: lens.el needs to be rewritten as a minor mode.
-(lens-mode-rebuild-mode) ;;needed so we don't get error: (void-variable lens-mode-map)
+;; TODO: rewrite as a minor mode
+(lens-mode-rebuild-mode) ;;needed so we don't get error:  (void-variable lens-mode-map)
 
 (define-key lens-mode-map [(f5)] 'lens-mode-rebuild-mode)
 
@@ -576,7 +500,7 @@ All terms less than this match only at the beginning of words (using `\\b')")
 
 (define-key lens-C-c-map [(control p)]
   (lambda () (interactive)
-	"Browse generated HTML."
+	"Preview generated HTML of this source file"
 	(save-buffer)
     (lens-make-page buffer-file-name)
     (browse-url-of-file
